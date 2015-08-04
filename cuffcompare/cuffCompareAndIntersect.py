54d3553a3d226451953b9ed8a4c42837a33f0931
ortiz-lab <crazyviolindude@gmail.com>
2015-08-04 15:09:58 -0700

    Added documentation for cuffCompareAndIntersect.

--- cuffcompare/cuffCompareAndIntersect.py
+++ cuffcompare/cuffCompareAndIntersect.py
@@ -15,13 +15,11 @@ def getCellLineFromFilename(f, delim = "_"):
     return line
 
 def getTranscriptID(longstring):
-    # Given a token from a .tracking file, extracts transcript ID
-    # Examples
+    # Given a token from a .tracking file (or any string), extracts transcript ID
+    # Examples of what we're looking for
     # Trinity: align_id:160849|GG14081
     # Cufflinks: CUFF.54.1
     # Stringtie: STRG.4.1
-    tokens = longstring.split("\t")
-
     if "CUFF" in longstring:
         match = re.search(r'CUFF\.[0-9]*\.[0-9]*', longstring)
     elif "STRG" in longstring:
@@ -30,12 +28,15 @@ def getTranscriptID(longstring):
         # Maybe GG is just because Genome-Guided? Maybe not best indentifier
         match = re.search(r'align_id:[0-9]*\|GG[0-9]*',longstring)
     else:
-        print("WARNING: Failed to find transcript ID in following token:")
+        print("WARNING: Failed to find transcript ID in following string:")
         print(longstring)
         return None
     return match.group(0)
 
 def runCuffDiff(*args):
+    # Runs cuffdiff on the given gtf files, alternating using one as reference
+    # and one as the query. Can handle either 2 or 3 .gtf files. Writes a log of
+    # commands executed.
     commandTemplate = "cuffcompare -s /media/Data/genomes/hg19_ordered/hg19.fa -o %s -r %s %s"
     prefixTemplate = "%s_%s_vs_%s_ref"
     commands = []
@@ -71,13 +72,12 @@ def runCuffDiff(*args):
         print("This program takes 2 or 3 input arguments. Each input must be a .gtf")
         print("file. We then use CuffCompare to do pairwise comparisons and return")
         print("a final merged .gtf. Order of input does not matter. Example:")
-        print("python cuffComareAndIntersect AV1_N_Cufflinks_Transcripts.gtf AV1_N_Trinity_Transcripts.gtf")
+        print("python cuffCompareAndIntersect AV1_N_Cufflinks_Transcripts.gtf AV1_N_Trinity_Transcripts.gtf")
         exit()
     filename = getCellLineFromFilename(args[0]) + "_cuffcompare_commands.log"
-    f = open(filename, mode = "w")
+    f = open(filename, mode = "w") # This is the log of commands
     for command in commands:
-        # subprocess.call(command, shell = True)
-        print("SKIPPING RUNNING CUFFDIFF FOR FASTER DEBUG")
+        subprocess.call(command, shell = True)
         f.write(command + "\n")
     f.close()
     return trackingFiles
@@ -95,7 +95,7 @@ def extractIDsFromTrackingFile(filename):
         content = f.readlines()
     for line in content:
         tokens = line.split("\t")
-        tf = tokens[3].lower()
+        tf = tokens[3].lower() # Select the column that is token, make sure is lowercase.
         if tf in tfCodes:
             reference = getTranscriptID(tokens[2])
             query = getTranscriptID(tokens[4])
@@ -128,14 +128,18 @@ def makeGtfFromIDs(cellLine, setOfIDs, sourceTools = ["Trinity", "Cufflinks", "S
         if os.path.isfile(origGtfName):
             with open(origGtfName, mode = 'r') as origGtf:
                 content = origGtf.readlines()
+            # Walk through each line in each of the original transcript files. If
+            # the line's ID matches one of the IDs given, then copy that line
+            # into the new output file. This method captures the transcript as
+            # well as the exons they contain, becuase we're traversing the
+            # original GTF file in order.
             for line in content:
-                if len(line) == 0 or line.isspace():
+                if len(line) == 0 or line.isspace(): # Skip empty lines
                     continue
                 thisID = getTranscriptID(line)
-                if thisID is None:
+                if thisID is None: # Skip lines that don't contain IDs
                     continue
                 if thisID in setOfIDs:
-                    # print(thisID)
                     f.write(line)
     f.close()
 
