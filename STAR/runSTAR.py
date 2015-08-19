476c4597475fef95b1becc214358baf896d91202
Kevin Wu <crazyviolindude@gmail.com>
2015-08-19 13:48:12 -0700

    Improve STAR documentation and general cleanup + refinement.

--- STAR/runSTAR.py
+++ STAR/runSTAR.py
@@ -1,9 +1,18 @@
 helpDoc = """This is a wrapper to run STAR. It takes in the following arguments:
 - fastq1 - the forward read fastq
-- fastq2 - the reverse read fastq (This specific order!)
-By default, we run without generating a custom genome indices. This increases
-accuracy at the cost of significantly longer computation times. However, we can
-enable such functionality by using the flag --generate-genome. Example usage:
+- fastq2 - the reverse read fastq
+Order of these is important!
+
+We output a .bam file that is sorted by coordinate. We also use STAR flags that
+make the output compatible with the Cuff suite of tools. 
+
+By default, we run without generating a custom genome indices. Genrating custom
+indices increases accuracy at the cost of significantly longer computation times.
+However, you can enable such functionality by using the flag --generate-genome. 
+Using this flag after already having run STAR once will RERUN STAR from the
+beginning.
+
+Example usage:
 python runSTAR.py [--generate-genome] x.fastq y.fastq
 
 Kevin Wu - Ortiz Lab UCSF - August 2015"""
@@ -16,24 +25,24 @@ sys.path.append("/home/ortiz-lab/Documents/kwu/scripts/util/")
 import fileUtil as f
 import shellUtil as s
 
-# print(f.longestCommonSubstring("Hello t here", "Hello there"))
-
 def runStar(fastq1, fastq2, genome = "/media/Data/genomes/STAR_index_hg19_vGATK/STAR_genomeDir_hg19_vGATK"):
 	# Runs STAR to output a coordinate soorted BAM file that is compatible with cuff
-	lcs = f.longestCommonSubstring(fastq1, fastq2)
+	lcs = f.longestCommonSubstring(fastq1, fastq2) # lcs = longest common substring
 	commandTemplate = "STAR --outSAMstrandField intronMotif --outFilterIntronMotifs RemoveNoncanonical --outSAMtype BAM SortedByCoordinate --genomeDir %s --readFilesIn %s %s --runThreadN 16 --outFileNamePrefix %s"
 	command = commandTemplate % (genome, fastq1, fastq2, lcs)
 	s.executeFunctions(command)
 
 def runStarGenome(fastq1, fastq2):
 	# STAR --runMode genomeGenerate --genomeDir $genomeDir2 --genomeFastaFiles $hg19dir --sjdbFileChrStartEnd $runDir/SJ.out.tab --sjdbOverhang 75 --runThreadN 16
-	genomeDir = "genome_generation"
-	s.executeFunctions("mkdir " + genomeDir)
+	genomeDir = f.longestCommonSubstring(fastq1, fastq2) + "_genome_generation"
+	s.executeFunctions("mkdir " + genomeDir) # Make the directory, so we can output to it.
 	inputSJ = f.longestCommonSubstring(fastq1, fastq2) + "_SJ.out.tab"
 	commandTemplate = "STAR --runMode genomeGenerate --genomeDir %s --genomeFastaFiles /media/Data/genomes/GATK_hg19/hg19/ucsc.hg19.fasta --sjdbFileChrStartEnd %s --sjdbOverhang 75 --runThreadN 16"
 	command = commandTemplate % (genomeDir, inputSJ)
+	return genomeDir # Return the directory the genome was written to so we can use in subsequent star run.
 
 def parseUserInput(args):
+	# Sanity check.
 	if len(args) > 3 or len(args) < 2:
 		print("Too few args")
 		print(helpDoc)
@@ -42,10 +51,13 @@ def parseUserInput(args):
 	if len(args) == 2:
 		runStar(args[0], args[1])
 	elif args[0] == "--generate-genome":
-		runStar(args[0], args[1])
-		runStarGenome(args[0], args[1])
-		runStar(args[0], args[1], "genome_generation")
-		exit()
+		runStar(args[0], args[1]) # Runs star once, get the SJ.out.tab file
+		gDir = runStarGenome(args[0], args[1]) # Runs the genome generatino using above output.
+		runStar(args[0], args[1], gDir) # Runs STAR again using custom genome.
+	else:
+		print("Unrecognized arguments.")
+		print(helpDoc)
+	exit()
 
 
 parseUserInput(sys.argv[1:])
