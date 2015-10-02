6ebb7aa117bc05bdf4ed944574ae5e088399d087
Kevin Wu <crazyviolindude@gmail.com>
2015-10-02 09:44:22 -0700

    Add TCGA utility functions.

---
+++ util/tcgaUtil.py
@@ -0,0 +1,15 @@
+import re
+import glob
+
+def getPrefixForIDs(ids, idmapfile = '/media/rawData/TCGA_SKCM/ids_to_filename.txt'):
+	idDict = dict()
+	idmap = open(idmapfile, mode = 'r')
+	for line in idmap:
+		parsed = l.split()
+		idDict[parsed[0] = parsed[1]]
+		# Stored as TCGA-XX-XXXX filenameprefix
+	idsList = ids.split()
+	prefixes = []
+	for i in idsList:
+		prefixes.append(idDict[i])
+	return prefixes
