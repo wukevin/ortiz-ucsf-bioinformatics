8b84fa1eae9643d79c65acc3d1eaf343202ed603
Kevin Wu <crazyviolindude@gmail.com>
2015-10-13 16:04:36 -0700

    Fixed missing import of shellutils

--- htseq/runHtseqCount.py
+++ htseq/runHtseqCount.py
@@ -2,6 +2,7 @@ import sys, os
 sys.path.append("/home/ortiz-lab/Documents/kwu/scripts/htseq/")
 sys.path.append("/home/ortiz-lab/Documents/kwu/scripts/util/")
 import htseq as h
+import shellUtil as s
 
 arguments = sys.argv[1:]
 
