34764d16cf831984c69b53ddb6b962f1bfb16539
ortiz-lab <crazyviolindude@gmail.com>
2016-04-27 12:59:16 -0700

    added function for renaming files to TCGA IDs

---
+++ util/rename_tcga.py
@@ -0,0 +1,53 @@
+import sys, os
+sys.path.append(os.environ['PIPELINEHOME'] + "/util/")
+import shellUtil as s
+import tcgaUtil as t
+import glob
+import getopt
+
+files = glob.glob("*.bam")
+# print(files)
+
+def fileRename(suffix, sim):
+    files = glob.glob("*" + suffix)
+    logfile = open("rename.mapping", mode = "w")
+    for f in files:
+        TCGA_ID = t.getMetadataFromSequenceFilename(f)
+        destination = TCGA_ID + suffix
+        logfile.write(f + '\t' + destination + '\n')
+        if sim:
+            continue
+        os.rename(f, destination)
+    logfile.close()
+
+# suffix = ".Aligned.soortedByCoord.bam"
+# logfile = open("rename.mapping", mode = "w") # Open to write
+# for f in files:
+#     TCGA_ID = t.getMetadataFromSequenceFilename(f)
+#     destination = TCGA_ID + suffix
+#     logfile.write(f + '\t' + destination + '\n')
+#     os.rename(f, destination)
+# logfile.close()
+
+
+def main():
+    try:
+        optlist, args = getopt.getopt(args = sys.argv[1:], shortopts = None, longopts = ['suffix=', 'simulate'])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    suffix = '.bam'
+    simulate = False
+    for o, a in optlist:
+        if o == '--suffix':
+            suffix = a
+        if o == 'simulate':
+            simulate = True
+        else:
+            print("Unrecognized argument.")
+            sys.exit(2)
+    fileRename(suffix, simulate)
+
+
+if __name__ == '__main__':
+    main()
